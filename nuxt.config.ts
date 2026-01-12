0c031514c52b4b0373ffb901eb41296f845b062f
Szymon Lukasik <sszymass@gmail.com>
2026-01-12 15:41:51 +0100

    vercel config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'vercel-static',
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
